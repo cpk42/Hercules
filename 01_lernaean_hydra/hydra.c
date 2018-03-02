b70937bce0377d3437ff223a63e2e30aaf4e5533
Curtis Krommenhoek <ckrommen@e1z3r3p23.42.us.org>
2018-03-01 19:41:45 -0800

    establishing connections

--- 01_lernaean_hydra/hydra.c
+++ 01_lernaean_hydra/hydra.c
@@ -5,42 +5,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 int main()
 {
 	pid_t pid;
+	pid_t sid;
 	char str[100];
 	int	listen_fd;
 	int	comm_fd;
 	
 	struct sockaddr_in servaddr; 
 	
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0); 
+
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+	servaddr.sin_port = htons(1024);
+
+	bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+	listen(listen_fd, 10);
+
+	comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+
 	pid = fork();
 	if (pid < 0)
 	{
 		printf("error\n");
 		exit(1);
 	}
-	listen_fd = socket(AF_INET, SOCK_STREAM, 0); 
-	
-	bzero( &servaddr, sizeof(servaddr));
-	
-	servaddr.sin_family = AF_INET;
-	
-	servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-
-	servaddr.sin_port = htons(1024);
-	
-	bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	
-	listen(listen_fd, 10);
-	
-	comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
-
+	if (pid > 0)
+		exit(0);
+	umask(0);
 	while (wait(&pid))
-    {
+	{
+		bzero(str, 100);
 		read(comm_fd, str, 100);
-		printf("pong pong\n\n");
-		write(comm_fd, str, strlen(str)+1);
-    }
+		if (!strncmp("ping", str, 4))
+			write(comm_fd, str, sizeof("pong pong\n"));
+	}
+	return (0);
 }
